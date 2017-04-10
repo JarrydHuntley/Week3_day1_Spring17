f490b4840faff43d308806694106af07a0f2662c
Jarryd Huntley <Jarryd Huntley>
2017-04-10 13:43:05 -0400

    mend

--- Week3_day1-Spring17/Car.cs
+++ Week3_day1-Spring17/Car.cs
@@ -59,7 +59,7 @@ namespace Week3_day1_Spring17
         public void PrintCarInfo()
         {
             Console.WriteLine("This car is a " + CarName + " that is " + color);
-            Console.WriteLine("It has " + numOfDoors + "doors and " + numOfSeats+ "seats");
+            Console.WriteLine("It has " + numOfDoors + " doors and " + numOfSeats+ " seats");
             Console.WriteLine("The VIN is " + VIN);
         }
 
