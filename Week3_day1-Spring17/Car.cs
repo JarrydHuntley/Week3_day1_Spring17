0647b1a3f39bcb851e6351fcc9bb8534ddda34cd
Jarryd Huntley <Jarryd Huntley>
2017-04-10 11:01:46 -0400

    Initial commit.

---
+++ Week3_day1-Spring17/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_day1_Spring17
+{
+    class Car
+    {
+        private string color;
+        private int numOfDoors;
+        private int numOfSeats;
+        private string make;
+        private string model;
+
+    }
+}
---
+++ Week3_day1-Spring17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_day1_Spring17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
